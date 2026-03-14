4de7e54da2005dea0ab7d78e458043c479a321fb
pupu08429-svg <pupu08429@gmail.com>
2026-03-14 22:26:14 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,48 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+	uint64_t max_value = 1;
+	while (num > 1) {
+		if (max_value < num) {
+			max_value = num;
+		}
+		if (num % 2 != 0) {
+			num = 3 * num + 1;
+		}
+		else {
+			num = num / 2;
+		}
+	}
+	return max_value;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+	unsigned int k = 1;
+	while (num > 1) {
+		if (num % 2 != 0) {
+			num = 3 * num + 1;
+		}
+		else {
+			num = num / 2;
+		}
+		k++;
+	}
+	return k;
 }
 
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
-  return 1;
+	unsigned int current_len = 0;
+	unsigned int max_len = 0;
+	uint64_t number = lbound;
+	for (uint64_t i = rbound; i >= lbound; i--) {
+		current_len = collatzLen(i);
+		if (current_len > max_len) {
+			max_len = current_len;
+			number = i;
+		}
+	}
+	*maxlen = max_len;
+	return number;
 }
